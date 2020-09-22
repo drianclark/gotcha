e6a6716a2ace5b6ec5de0cdaa1e133ba69bc63ed
drianclark <dreiclark@gmail.com>
2020-09-22 21:16:01 +0100

    dynamic server url across envs

--- react-ui/src/socket.ts
+++ react-ui/src/socket.ts
@@ -2,6 +2,8 @@ import socketIOClient from "socket.io-client";
 
 declare var filterXSS: any;
 
-const serverURL = process.env.SERVER_URL ? process.env.SERVER_URL : 'http://localhost:5000'
+const serverURL = process.env.SERVER_URL || 'http://localhost:5000'
+
+console.log(process.env);
 
 export const socket = socketIOClient(serverURL);
